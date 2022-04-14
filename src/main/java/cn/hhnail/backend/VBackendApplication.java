696be2d944488eac5d9df6a3062213dd2ec6aafc
hhnail <hhnail@163.com>
2022-04-14 19:51:06 +0800

    project init

---
+++ src/main/java/cn/hhnail/backend/VBackendApplication.java
@@ -0,0 +1,13 @@
+package cn.hhnail.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/hhnail/backend/VBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hhnail.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
