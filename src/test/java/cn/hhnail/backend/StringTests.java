4d453d886dfe633e258639617dbf4f504a309323
lsHhnail <hhnail@163.com>
2022-06-01 17:11:53 +0800

    [前端]动态加载Descriptions

--- src/test/java/cn/hhnail/backend/StringTests.java
+++ src/test/java/cn/hhnail/backend/StringTests.java
@@ -1,8 +1,15 @@
 package cn.hhnail.backend;
 
+import org.junit.jupiter.api.Test;
+
 public class StringTests {
 	public static void main(String[] args) {
 		String str1 = "[\"id\":\"123\"]";
 		System.out.println(str1);
+	}
+
+	@Test
+	public void test01(){
+		System.out.println(null + "");
 	}
 }
