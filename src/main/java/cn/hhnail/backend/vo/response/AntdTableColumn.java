02d2d7f268d50528fd71a6f82dce5995498519e9
Hhnail <779943749@qq.com>
2022-10-23 22:28:26 +0800

    各种字段的解释

--- src/main/java/cn/hhnail/backend/vo/response/AntdTableColumn.java
+++ src/main/java/cn/hhnail/backend/vo/response/AntdTableColumn.java
@@ -9,14 +9,10 @@ import java.io.Serializable;
 @ToString
 public class AntdTableColumn implements Serializable {
 
-    private String title;
-
-    private String dataIndex;
-
-    private String columnCode;
-
-    private String key;
-
-    private Integer width;
+    private String title; // 字段中文名称
+    private String dataIndex; // 字段（在UI组件中）数据匹配字段的唯一标识
+    private String columnCode; // 真实的数据库字段名
+    private String key; // 字段（在UI组件中）用于Diffing算法的唯一标识
+    private Integer width;  // 字段（在UI组件中）展示的列宽度
 
 }
