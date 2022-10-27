f19bd420046b9650ea09f7c7a4c4d4ce81aa5fab
penghangwei <hhnail@163.com>
2022-10-27 16:36:56 +0800

    EasyExcel写入demo

---
+++ src/main/java/cn/hhnail/backend/controller/FileController.java
@@ -0,0 +1,51 @@
+package cn.hhnail.backend.controller;
+
+
+import cn.hhnail.backend.bean.DemoData;
+import cn.hhnail.backend.vo.response.AppResponse;
+import com.alibaba.excel.EasyExcel;
+import org.junit.Test;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping("/vapi")
+public class FileController {
+
+    @RequestMapping(method = RequestMethod.POST, value = "/uploadFile")
+    public AppResponse<String> uploadFile(@RequestParam("file") MultipartFile file) {
+
+        System.out.println(file);
+
+        return AppResponse.ok(null);
+    }
+
+
+    private List<DemoData> data() {
+        List<DemoData> testData = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            DemoData item = new DemoData();
+            item.setString("第" + i + "行");
+            item.setDoubleData((double) i);
+            item.setDate(new Date());
+            testData.add(item);
+        }
+        return testData;
+    }
+
+
+    /**
+     * 写入Excel
+     */
+    @Test
+    public void simpleWrite() {
+        String fileName = "D:\\" + "test.xlsx";
+        EasyExcel.write(fileName, DemoData.class)
+                .sheet("模板")
+                .doWrite(data());
+    }
+}
