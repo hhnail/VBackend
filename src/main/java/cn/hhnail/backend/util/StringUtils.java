722f5c3dd24273faf1dd147ee05f2b46cc942200
Hhnail <hhnail@163.com>
2023-03-07 13:19:39 +0800

    StringUtils safeEquals 能避免NPE的equals

--- src/main/java/cn/hhnail/backend/util/StringUtils.java
+++ src/main/java/cn/hhnail/backend/util/StringUtils.java
@@ -141,4 +141,22 @@ public class StringUtils {
         result.append(str);
         return result.toString();
     }
+
+    /**
+     * 安全的Equals
+     * 避免NPE
+     *
+     * @param str1
+     * @param str2
+     * @return
+     */
+    public static boolean safeEquals(String str1, String str2) {
+        if (str1 == null) {
+            str1 = "";
+        }
+        if (str2 == null) {
+            str2 = "";
+        }
+        return str1.equals(str2);
+    }
 }
